8846e1f70de808c48048b251f68fe11327b92f6a
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-08-06 15:55:40 -0400

    Added new deps

---
+++ bower_components/Trip.js/src/themes/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  black: require('./default'),
+  dark: require('./default'),
+  white: require('./default'),
+  yeti: require('./default'),
+  default: require('./default'),
+  minimalism: require('./minimalism')
+};
