95a9adf41937569779c64bcaadfbbaae4997a8b3
Apoorv Verma [AP] <avdaredevil@gmail.com>
2018-10-19 14:45:48 -0700

    HTML Refactor

--- highcharts-map.js
+++ highcharts-map.js
@@ -11,18 +11,16 @@ import {HighchartsPolymer} from './highcharts-behavior.js'
 */
 class HighchartsMap extends HighchartsPolymer.BaseBehavior(PolymerElement) {
     static get is() {return "highcharts-map"}
-    static get template() {
-        return html`
-            <style>
-                :host {width: 100%;display: inline-block;min-height: var(--highcharts-min-height, 26em)}
-                :host[height-responsive] {min-height: initial}
-                :host[height-responsive] #Chart {height: 100%}
-                #Chart {@apply --highcharts-container}
-            </style>
-            <div id="Chart" width="100%"></div>
-            <slot></slot>
-        `
-    }
+    static get template() {return html`
+        <style>
+            :host {width: 100%;display: inline-block;min-height: var(--highcharts-min-height, 26em)}
+            :host[height-responsive] {min-height: initial}
+            :host[height-responsive] #Chart {height: 100%}
+            #Chart {@apply --highcharts-container}
+        </style>
+        <div id="Chart" width="100%"></div>
+        <slot></slot>
+    `}
     static get properties() {return {
         type: {type: String, value: 'line', observer: '_updateType'}
     }}
