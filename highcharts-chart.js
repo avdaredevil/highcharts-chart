5c50116189f952fe530bb4b0f55c14ee6f6e2538
Apoorv Verma [AP] <avdaredevil@gmail.com>
2018-10-19 14:45:21 -0700

    HTML Refactor

--- highcharts-chart.js
+++ highcharts-chart.js
@@ -11,18 +11,16 @@ import {HighchartsPolymer} from './highcharts-behavior.js'
 */
 class HighchartsChart extends HighchartsPolymer.ChartBehavior(HighchartsPolymer.BaseBehavior(PolymerElement)) {
     static get is() {return "highcharts-chart"}
-    static get template() {
-        return html`
-            <style>
-                :host {width: 100%;display: inline-block;min-height: var(--highcharts-min-height, 26em)}
-                :host[height-responsive] {min-height: initial}
-                :host[height-responsive] #Chart {height: 100%}
-                #Chart {@apply --highcharts-container}
-            </style>
-            <div id="Chart" width="100%" on-click="_checkSelected"></div>
-            <slot></slot>
-        `
-    }
+    static get template() {return html`
+        <style>
+            :host {width: 100%;display: inline-block;min-height: var(--highcharts-min-height, 26em)}
+            :host[height-responsive] {min-height: initial}
+            :host[height-responsive] #Chart {height: 100%}
+            #Chart {@apply --highcharts-container}
+        </style>
+        <div id="Chart" width="100%" on-click="_checkSelected"></div>
+        <slot></slot>
+    `}
     static get properties() {return {
         type: {type: String, value: 'line', observer: '_updateType'}
     }}
