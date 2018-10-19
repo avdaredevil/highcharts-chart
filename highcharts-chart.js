b0ee13e9e31f845f42a52072665fae3c8e4d4e01
Apoorv Verma [AP] <avdaredevil@gmail.com>
2018-10-19 14:42:30 -0700

    Consistency

--- highcharts-chart.js
+++ highcharts-chart.js
@@ -1,5 +1,5 @@
-import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
-import { HighchartsPolymer } from './highcharts-behavior.js';
+import {PolymerElement, html} from '@polymer/polymer/polymer-element.js'
+import {HighchartsPolymer} from './highcharts-behavior.js'
 
 /**
 * @polymer
@@ -13,12 +13,14 @@ class HighchartsChart extends HighchartsPolymer.ChartBehavior(HighchartsPolymer.
     static get is() {return "highcharts-chart"}
     static get template() {
         return html`
-    <style>
-        :host {width: 100%;display: inline-block;min-height: var(--highcharts-min-height, 26em)}
-        :host[height-responsive] {min-height: initial}
-        :host[height-responsive] #Chart {height: 100%}
-        #Chart {@apply --highcharts-container}
-    </style><div id="Chart" width="100%" on-click="_checkSelected"></div>
+            <style>
+                :host {width: 100%;display: inline-block;min-height: var(--highcharts-min-height, 26em)}
+                :host[height-responsive] {min-height: initial}
+                :host[height-responsive] #Chart {height: 100%}
+                #Chart {@apply --highcharts-container}
+            </style>
+            <div id="Chart" width="100%" on-click="_checkSelected"></div>
+            <slot></slot>
         `
     }
     static get properties() {
