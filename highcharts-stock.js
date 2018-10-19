2281ca5fba364bc86d3ab6bd7e6dbbb6c4b1b23f
Apoorv Verma [AP] <avdaredevil@gmail.com>
2018-10-19 14:40:17 -0700

    Update highcharts-stock.js

--- highcharts-stock.js
+++ highcharts-stock.js
@@ -1,5 +1,5 @@
 import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
-import { HighchartsPolymer } from './highcharts-behavior.js';
+import {HighchartsPolymer} from './highcharts-behavior.js';
 
 /**
 * @polymer
@@ -12,13 +12,13 @@ class HighchartsStock extends HighchartsPolymer.ChartBehavior(HighchartsPolymer.
     static get is() {return "highcharts-stock"}
     static get template() {
         return html`
-<style>
-    :host {width: 100%;display: inline-block;min-height: var(--highcharts-min-height, 26em)}
-    :host[height-responsive] {min-height: initial}
-    :host[height-responsive] #Chart {height: 100%}
-    #Chart {@apply --highcharts-container}
-</style>
-<div id="Chart" width="100%" on-click="_checkSelected"></div>
+            <style>
+                :host {width: 100%;display: inline-block;min-height: var(--highcharts-min-height, 26em)}
+                :host[height-responsive] {min-height: initial}
+                :host[height-responsive] #Chart {height: 100%}
+                #Chart {@apply --highcharts-container}
+            </style>
+            <div id="Chart" width="100%" on-click="_checkSelected"></div>
         `
     }
     static get properties() {
